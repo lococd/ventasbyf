b603a88dd35533858af44459dbb0fb82ee56afb4
oliveroliva <oliveroliva@hotmail.com>
2026-01-07 02:07:32 -0300

    fix notas enviadas

--- www/js/vernotasenviadas.js
+++ www/js/vernotasenviadas.js
@@ -86,50 +86,97 @@ document.addEventListener('deviceready', function(){
 
 	    	// listo todos los ficheros
 	    	directoryReader.readEntries(function(entries) {
-	                                    	var i;
+											// sort entries by date ascending (try metadata first, fall back to parsing filename)
+											// sort entries by date ascending (try metadata first, fall back to parsing filename)
+											function getTime(entry) {
+												if (!entry || !entry.name) return 0;
+												var m = entry.name.match(/\d{8}/);
+												if (m) {
+													var s = m[0];
+													var y = parseInt(s.substring(0, 4), 10);
+													var mo = parseInt(s.substring(4, 6), 10) - 1;
+													var d = parseInt(s.substring(6, 8), 10);
+													return new Date(y, mo, d).getTime();
+												}
+												return 0;
+											}
+
+											function compareEntries(a, b) {
+												var ta = getTime(a);
+												var tb = getTime(b);
+												if (ta !== tb) return ta - tb;
+												var na = (a.name || "").toLowerCase();
+												var nb = (b.name || "").toLowerCase();
+												if (na < nb) return -1;
+												if (na > nb) return 1;
+												return 0;
+											}
+
+											// insertion sort (stable) — avoids using Array.prototype.sort
+											for (var i = 1; i < entries.length; i++) {
+												var key = entries[i];
+												var j = i - 1;
+												while (j >= 0 && compareEntries(entries[j], key) > 0) {
+													entries[j + 1] = entries[j];
+													j--;
+												}
+												entries[j + 1] = key;
+											}
+											var i;
 	                                    	var nombreArchivo = "";
 	                                    	var celdas = "";
 	                                    	for (i=0; i<entries.length; i++) {
 	                                    		nombreArchivo = entries[i].name;
-	                                    		window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory + "nvtEnviadas/" + nombreArchivo,
-	                                    		gotFile, fail);
-	                                    		function gotFile(fileEntry) {
-													fileEntry.file(function(file) {
-														var reader = new FileReader();
-														reader.onloadend = function(e) {
-															celdas = "";
-															var parser = new DOMParser();
-															var xmlDoc = parser.parseFromString(this.result,"text/xml");
-															var totneto = xmlDoc.getElementsByTagName("totgen")[0].childNodes[0].nodeValue;
-															var razons = xmlDoc.getElementsByTagName("razons")[0].childNodes[0].nodeValue;
-															var fecemiRaw = xmlDoc.getElementsByTagName("fecemi")[0].childNodes[0].nodeValue;
-															var fecemi = "";
-															if (fecemiRaw && fecemiRaw.length === 8) {
-																// yyyymmdd -> dd/mm/yy
-																var year = fecemiRaw.substring(2, 4); // get last two digits
-																var month = fecemiRaw.substring(4, 6);
-																var day = fecemiRaw.substring(6, 8);
-																fecemi = day + "/" + month + "/" + year;
-															}
-															else {
-																fecemi = fecemiRaw;
-															}
-															celdas = celdas + "<tr>" +
-															"<td>" + fecemi + "</td>" +
-															'<td class="text-right">$' + totneto + "</td>" +
-				                                    		'<td></td>' +
-															'<td><a href="#" class="btn btn-primary" onclick="visualizarNota(\''+ file.name +'\',\'nvtEnviadas\')">Ver</a></td>' +
-															'</tr>'+
-															'<tr><td colspan="4"><b>' + razons + '</b></td></tr>';
-				                                    		$("#detalleTblNotasEnviadas").append(celdas);
+												// process entries sequentially starting from current index
+												(function processFrom(idx){
+													if (idx >= entries.length) return;
+													var name = entries[idx].name;
+													window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory + "nvtEnviadas/" + name,
+														function(fileEntry){
+															fileEntry.file(function(file){
+																var reader = new FileReader();
+																reader.onloadend = function(e){
+																	var celdas = "";
+																	var parser = new DOMParser();
+																	var xmlDoc = parser.parseFromString(this.result,"text/xml");
+																	var totneto = xmlDoc.getElementsByTagName("totgen")[0].childNodes[0].nodeValue;
+																	var razons = xmlDoc.getElementsByTagName("razons")[0].childNodes[0].nodeValue;
+																	var fecemiRaw = xmlDoc.getElementsByTagName("fecemi")[0].childNodes[0].nodeValue;
+																	var fecemi = "";
+																	if (fecemiRaw && fecemiRaw.length === 8) {
+																		var year = fecemiRaw.substring(2, 4);
+																		var month = fecemiRaw.substring(4, 6);
+																		var day = fecemiRaw.substring(6, 8);
+																		fecemi = day + "/" + month + "/" + year;
+																	} else {
+																		fecemi = fecemiRaw;
+																	}
+																	celdas = celdas + "<tr>" +
+																		"<td>" + fecemi + "</td>" +
+																		'<td class="text-right">$' + totneto + "</td>" +
+																		'<td></td>' +
+																		'<td><a href="#" class="btn btn-primary" onclick="visualizarNota(\''+ file.name +'\',\'nvtEnviadas\')">Ver</a></td>' +
+																		'</tr>' +
+																		'<tr><td colspan="4"><b>' + razons + '</b></td></tr>';
+																	$("#detalleTblNotasEnviadas").append(celdas);
+																	// proceed to next entry after this one finished processing
+																	processFrom(idx + 1);
+																};
+																reader.readAsText(file);
+															}, function(err){
+																// on file read error, continue to next
+																alert("File read error: " + (err && err.code));
+																processFrom(idx + 1);
+															});
+														},
+														function(err){
+															alert("File system error: " + (err && err.code));
+															processFrom(idx + 1);
 														}
-														reader.readAsText(file);
-													});
-												}
-
-												function fail(e) {
-													alert("FileSystem Error" + e.message);
-												}
+													);
+												})(i);
+												// stop the outer for-loop from continuing; processing continues in the recursive function
+												i = entries.length;
 	                                      }
 	                                  }
 	      	,function fail(error) {
